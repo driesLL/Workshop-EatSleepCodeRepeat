bcbe98eff8cfadf349ed1b436e8680ddd56cc0f7
driesLL <User@PC85>
2016-10-05 14:25:13 +0200

    Lap2 #1

--- Lab2/CheckingAccount.cs
+++ Lab2/CheckingAccount.cs
@@ -7,22 +7,14 @@ namespace eu.sig.training.ch04.v1
 
         public Transfer MakeTransfer(String counterAccount, Money amount)
         {
-            // 1. Check withdrawal limit:
-            if (amount.GreaterThan(this.transferLimit))
-            {
-                throw new BusinessException("Limit exceeded!");
-            }
+            CheckGreaterThanLimit(amount);
+            ValidateLength(counterAccount);
+            int sum = CalcSum(counterAccount);
+            return LastCheck(counterAccount, amount, sum);
+        }
 
-            // 2. Assuming result is 9-digit bank account number, validate 11-test:
-            if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9)
-            {
-                throw new BusinessException("Invalid account number!");
-            }
-            int sum = 0;
-            for (int i = 0; i < counterAccount.Length; i++)
-            {
-                sum = sum + (9 - i) * (int)Char.GetNumericValue(counterAccount[i]);
-            }
+        private Transfer LastCheck(string counterAccount, Money amount, int sum)
+        {
             if (sum % 11 == 0)
             {
                 // 3. Look up counter account and make transfer object:
@@ -33,6 +25,35 @@ namespace eu.sig.training.ch04.v1
             else
             {
                 throw new BusinessException("Invalid account number!");
+            }
+        }
+
+        private static int CalcSum(string counterAccount)
+        {
+            int sum = 0;
+            for (int i = 0; i < counterAccount.Length; i++)
+            {
+                sum = sum + (9 - i) * (int)Char.GetNumericValue(counterAccount[i]);
+            }
+
+            return sum;
+        }
+
+        private static void ValidateLength(string counterAccount)
+        {
+            // 2. Assuming result is 9-digit bank account number, validate 11-test:
+            if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9)
+            {
+                throw new BusinessException("Invalid account number!");
+            }
+        }
+
+        private void CheckGreaterThanLimit(Money amount)
+        {
+            // 1. Check withdrawal limit:
+            if (amount.GreaterThan(this.transferLimit))
+            {
+                throw new BusinessException("Limit exceeded!");
             }
         }
     }
--- Lab2/SavingsAccount.cs
+++ Lab2/SavingsAccount.cs
@@ -6,37 +6,60 @@ namespace eu.sig.training.ch04.v1
     {
         public CheckingAccount RegisteredCounterAccount { get; set; }
 
-        public Transfer makeTransfer(string counterAccount, Money amount)
+        public Transfer makeTransfer(string counterAccount, Money    amount)
+        {
+            Validate(counterAccount);
+            int sum = CalcSum(counterAccount);
+            return LastValidate(counterAccount, amount, sum);
+        }
+
+        private Transfer LastValidate(string counterAccount, Money amount, int sum)
+        {
+            if (sum % 11 == 0)
+            {
+                // 2. Look up counter account and make transfer object:
+                CheckingAccount acct = Accounts.FindAcctByNumber(counterAccount);
+                Transfer result = new Transfer(this, acct, amount); // <2>
+                                                                    // 3. Check whether withdrawal is to registered counter account:
+                ValidateThis(result);
+                return result;
+            }
+            else
+            {
+                throw new BusinessException("Invalid account number!!");
+            }
+        }
+
+        private void ValidateThis(Transfer result)
+        {
+            if (result.CounterAccount.Equals(this.RegisteredCounterAccount))
+            {
+                return;
+            }
+            else
+            {
+                throw new BusinessException("Counter-account not registered!");
+            }
+        }
+
+        private static void Validate(string counterAccount)
         {
             // 1. Assuming result is 9-digit bank account number, validate 11-test:
             if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9)
             {
                 throw new BusinessException("Invalid account number!");
             }
-            int sum = 0;
+        }
+
+        private static int CalcSum(string counterAccount)
+        {
+            var sum = 0;
             for (int i = 0; i < counterAccount.Length; i++)
             {
                 sum = sum + (9 - i) * (int)Char.GetNumericValue(counterAccount[i]);
             }
-            if (sum % 11 == 0)
-            {
-                // 2. Look up counter account and make transfer object:
-                CheckingAccount acct = Accounts.FindAcctByNumber(counterAccount);
-                Transfer result = new Transfer(this, acct, amount); // <2>
-                // 3. Check whether withdrawal is to registered counter account:
-                if (result.CounterAccount.Equals(this.RegisteredCounterAccount))
-                {
-                    return result;
-                }
-                else
-                {
-                    throw new BusinessException("Counter-account not registered!");
-                }
-            }
-            else
-            {
-                throw new BusinessException("Invalid account number!!");
-            }
+
+            return sum;
         }
     }
 }
